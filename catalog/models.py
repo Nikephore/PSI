a1cb6c3098a35eadbc5cc7a7e97e8fa543587683
bernardo <bernarrr69@gmail.com>
2021-11-09 16:36:55 +0100

    cosas mejoradas de las clases

--- catalog/models.py
+++ catalog/models.py
@@ -13,6 +13,9 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 
 class Book(models.Model):
     book_id = models.IntegerField(primary_key= True)
@@ -24,22 +27,23 @@ class Book(models.Model):
     date = models.DateField()
     score = models.DecimalField()
     slug = models.SlugField()
+    author_book = models.ManyToManyField(Author, help_text="Introduzca un autor para este libro")
 
-class AuthorBook(models.Model): 
-    book_id = ForeignKey(Book, on_delete=models.SET_NULL)
-    author_id = ForeignKey(Author, on_delete=models.SET_NULL)
 
-    class Meta:
-        unique_together = ('book_id', 'author_id')
+    def __str__(self):
+        return self.title
 
 
 
 class Comment(models.Model):
     comment_id = models.IntegerField(primary_key= True)
     book_id = ForeignKey(Book, on_delete=models.SET_NULL)
-    user_id = ForeignKey(Book, on_delete=models.SET_NULL)
+    user_id = ForeignKey(User, on_delete=models.SET_NULL)
     date = models.DateField()
     msg = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.msg
 
 
 
