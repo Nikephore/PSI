6e2795820b4ea2ca16ef95439c2d1608c52d4e17
bernardo <bernarrr69@gmail.com>
2021-11-10 19:04:25 +0100

    Semanas 1 y 2 acabadas lesgou

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
-from bookshop import views as core_views
-
+from . import views
 urlpatterns = [
-    url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
