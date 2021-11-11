ef3408a082ec81c3975e2e642028525ff0396291
Nikephore <nuciforaluis@gmail.com>
2021-11-11 18:17:40 +0100

    settings database

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,12 +75,20 @@ WSGI_APPLICATION = 'bookshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+# The following environment variable, called DATABASE_URL, has to be defined
+# at the o.s. level: export DATABASE_URL =
+# ’postgres://alumnodb:alumnodb@localhost:5432/psi’
+
+# export DATABASE_URL = 'postgres://alumnodb:alumnodb@localhost:5432/psi'
+DATABASES = {}
+if os.getenv('SQLITE', False):
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+else:
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(default='postgres://alumnodb:alumnodb@localhost:5432/psi', conn_max_age=500)
 
 
 # Password validation
