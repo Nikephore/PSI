0b8d2f7da8157f8376dcae35de8d8a5909e572b5
Nikephore <nuciforaluis@gmail.com>
2021-11-11 13:13:02 +0100

    admin classes

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,41 @@
 from django.contrib import admin
 
-from .models import Author,  Book, Comment
+from .models import Author, Book, Comment
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Comment)
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
+    inlines = [BooksInline]
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class AuthorsInLine(admin.TabularInline):
+    model = Author
+    extra = 0
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isbn' )
+    inLines = [AuthorsInLine, CommentsInline]
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Comment, CommentAdmin)
