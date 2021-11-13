9e153c7a91ba23ea7c50ce1bd9a32f77968ad5f1
Nikephore <nuciforaluis@gmail.com>
2021-11-13 19:33:55 +0100

    se muestra en home las listas de libros bien

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 import datetime
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -15,24 +16,25 @@ from .models import Book, Author
 
 
 def home(request):
-    """
-    Función vista para la página inicio del sitio.
-    """
+    score_order = Book.objects.all().order_by('-score')[:5]
+    date_order = Book.objects.all().order_by('-date')[:5]
+
+    context = {'score_order' : score_order, 'date_order' : date_order}
 
     # Renderiza la plantilla HTML home.html con los datos en la variable contexto
-    return render(request, 'home.html', context=None)
+    return render(request, 'home.html', context=context)
 
 
 def BookDetailView(request, slug):
     sl=(get_object_or_404(Book, slug=slug))
     return render(request, "catalog/book_detail.html", {"book" : sl})
 
-  
-class BookListView(generic.ListView):
-    model = Book
-    paginate_by = 5
-
 
 class AuthorListView(generic.ListView):
     model = Author
+    paginate_by = 5
+
+
+class BookListView(generic.ListView):
+    model = Book
     paginate_by = 5
