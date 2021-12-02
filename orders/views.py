4234985dac84b8f6bb22b7572375cff829f020ce
bernardo <bernarrr69@gmail.com>
2021-12-02 15:47:33 +0100

    Empezamos con los forms y las views de cart

--- orders/views.py
+++ orders/views.py
@@ -1,2 +1,18 @@
+from orders.forms import CartAddBookForm
+from .cart import Cart
+from django.shortcuts import render, redirect
+
+
 class BaseCart():
     template_name = 'cart.html'
+
+def cart_add(request, book_slug):
+    cart = Cart(request)
+    # Procesar el formulario para obtener la unidades y anyadirlas
+    form = CartAddBookForm(request)
+    if form.is_valid():
+        form.save()
+        units = form.units
+        cart.add(obtener_libro, quantity=units)
+        return redirect('cart_list')
+    return redirect('cart_list')
