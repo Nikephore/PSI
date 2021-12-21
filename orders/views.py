13777411adf5f4c16058ab457a153b2bb106a433
bernardo <bernarrr69@gmail.com>
2021-12-21 19:01:48 +0100

    flake8

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,13 @@
 import datetime
 
-from django.http import request
 from orders.forms import CartAddBookForm, OrderCreateForm
 from django.views import generic
-from orders.tests_services import FIRSTNAME
-from .cart import Cart 
+from .cart import Cart
 from catalog.models import Book
 from django.conf import settings
 from django.shortcuts import render, redirect
 from .models import Order, OrderItem
+
 
 class BaseCart(generic.ListView):
     context_object_name = 'cart_list'
@@ -18,13 +17,15 @@ class BaseCart(generic.ListView):
         cart_list = self.request.session[settings.CART_SESSION_ID]
         return cart_list
 
+
 def order_created(request):
     return render(request, "orders/checkout.html", context=None)
+
 
 def cart_add(request, slug):
     cart = Cart(request)
     if request.method == 'POST':
-    # Procesar el formulario para obtener la unidades y anyadirlas
+        # Procesar el formulario para obtener la unidades y anyadirlas
         form = CartAddBookForm(request.POST)
         if form.is_valid():
             units = int(form.cleaned_data.get('quantity'))
@@ -33,13 +34,15 @@ def cart_add(request, slug):
         return redirect('cart_list')
     return redirect('cart_list')
 
+
 def cart_remove(request, slug):
     cart = Cart(request)
     if request.method == 'GET':
-    # Procesar el formulario para obtener la unidades y anyadirlas
+        # Procesar el formulario para obtener la unidades y anyadirlas
         sl = Book.objects.get(slug=slug)
         cart.remove(sl)
     return redirect('cart_list')
+
 
 def cart_clear(request):
     cart = Cart(request)
@@ -47,12 +50,14 @@ def cart_clear(request):
 
     return redirect('home')
 
+
 def order_create(request):
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
-        return render(request, 'orders/checkoutform.html', context= {'form': form})
+        return render(request, 'orders/checkoutform.html', context={'form': form})
 
-def order_process(request):      
+
+def order_process(request):
     fn = request.POST['first_name']
     ln = request.POST['last_name']
     e = request.POST['email']
@@ -61,15 +66,15 @@ def order_process(request):
     pc = request.POST['postal_code']
 
     orde = Order.objects.create(
-        first_name = fn,
-        last_name = ln,
-        email = e,
-        address = a,
-        city = c,
-        postal_code = pc,
-        created = datetime.date.today(),
-        updated = datetime.date.today(),
-        paid = True
+        first_name=fn,
+        last_name=ln,
+        email=e,
+        address=a,
+        city=c,
+        postal_code=pc,
+        created=datetime.date.today(),
+        updated=datetime.date.today(),
+        paid=True
     )
     orde.save()
 
@@ -82,13 +87,13 @@ def order_process(request):
         q = item['quantity']
         p = item['price']
         oi = OrderItem.objects.create(
-            book = bo,
-            order = order,
-            quantity = q,
-            price = p
+            book=bo,
+            order=order,
+            quantity=q,
+            price=p
         )
         oi.save()
-    
+
     cart.clear()
 
     return redirect('order_created')
